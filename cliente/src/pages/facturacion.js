d51caffaf657cdc295804df5466e5cc17f2ffbb8
jose491390 <joseeduardolondonoalvarez4913@gmail.com>
2024-05-16 17:29:22 -0500

    caombios de estilo

--- cliente/src/pages/facturacion.js
+++ cliente/src/pages/facturacion.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-import '../css/estilos-facturacion.css';
+import "../css/estilos-facturacion.css";
+import Swal from "sweetalert2";
 
 function Facturacion({ nombre }) {
   //aqui se definen todas las constantes que se usaran en la interfaz de facturacion
@@ -14,11 +15,25 @@ function Facturacion({ nombre }) {
   const [productosSeleccionados, setProductosSeleccionados] = useState([]);
   const [busqueda, setBusqueda] = useState("");
 
+  const [empresa, setEmpresa] = useState({});
+
+  // Función para obtener la información de la empresa
+  const obtenerEmpresa = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3000/api/config/empresa"
+      );
+      setEmpresa(response.data);
+    } catch (error) {
+      console.error("Error al obtener los datos de la empresa:", error);
+    }
+  };
+
   const [cliente, setCliente] = useState({
     nit: "",
     nombre: "",
     direccion: "",
-    telefono: ""
+    telefono: "",
   });
 
   const [nitInput, setNitInput] = useState("");
@@ -52,7 +67,9 @@ function Facturacion({ nombre }) {
         await axios.post("http://localhost:3000/api/config/numActual", {
           numActual: numInicio,
         });
-        const response = await axios.get("http://localhost:3000/api/config/numActual");
+        const response = await axios.get(
+          "http://localhost:3000/api/config/numActual"
+        );
         const data = response.data[0];
         setNumInicio(data.numActual);
         setNumActual(`${autoriza} - 0000${numInicio}`);
@@ -100,37 +117,57 @@ function Facturacion({ nombre }) {
     }
   };
 
-  const buscarClientePorNit = async (nit) =>{
+  const buscarClientePorNit = async (nit) => {
     try {
-      const response = await axios.get(`http://localhost:3000/api/cliente/${nit}`);
+      const response = await axios.get(
+        `http://localhost:3000/api/cliente/${nit}`
+      );
       const clienteEncontrado = response.data;
 
-      if(clienteEncontrado){
+      if (clienteEncontrado) {
         //actualiza el estado del cliente encontrado con sus datos
         setCliente({
           nombre: clienteEncontrado.nombre,
           direccion: clienteEncontrado.direccion,
           telefono: clienteEncontrado.telefono,
-          // almacenar solo el objeto del cliente 
+          // almacenar solo el objeto del cliente
           _id: clienteEncontrado._id,
         });
-      }else{
-        // si no encuentra ningun cliente con el NIT especifico, limpiar los datos del cliente
-        setCliente({
-          nombre: "",
-          direccion: "",
-          telefono: "",
-          _id: "", // Limpiar el ObjectId del cliente
-        });
-        console.log("cliente no encontrado");
-        alert("cliente no encontrado ");
-      }      
+      }
     } catch (error) {
-      console.log("eeror al buscar cliente", error)      
+      console.error("Error al buscar cliente:", error);
+      // En caso de error, también establecer los datos del cliente en blanco
+      setCliente({
+        nombre: "",
+        direccion: "",
+        telefono: "",
+        _id: "",
+      });
+      setNitInput("");
+      Swal.fire({
+        title: "cliente no encontrado!",
+        html:
+          "<i>el cliente  <strong> " +
+          nitInput +
+          "</strong> no esta  registrado  </i>",
+        icon: "error",
+        timer: 3000,
+      });
     }
-  }
+  };
 
   const hasdleBuscarCliente = () => {
+    // Verificar si el campo de NIT está vacío
+    if (!nitInput.trim()) {
+      // Si el campo está vacío, establecer los datos del cliente en blanco
+      setCliente({
+        nombre: "",
+        direccion: "",
+        telefono: "",
+        _id: "",
+      });
+    }
+
     //llama la funcion para buscarl el cliente
     buscarClientePorNit(nitInput);
   };
@@ -139,127 +176,125 @@ function Facturacion({ nombre }) {
     try {
       let url = `http://localhost:3000/api/producto/`;
 
-      if(!isNaN(busqueda)){
+      if (!isNaN(busqueda)) {
         url += `?codigo=${busqueda}`;
-      }else{
+      } else {
         url += `${busqueda}`;
       }
 
       const response = await axios.get(url);
-      setProductos(response.data);      
+      setProductos(response.data);
     } catch (error) {
       console.error("error al buscar el producto", error);
       setProductos([]);
-      
     }
-  }
+  };
 
   const agregarProducto = (producto) => {
     const total = producto.precio * producto.cantidad;
     const productoConTotal = { ...producto, total };
     const productoConCantidad = { ...producto, cantidad: 0 };
     setProductosSeleccionados([...productosSeleccionados, productoConTotal]);
-  }
+  };
 
   const handleCantidadChange = (index, cantidad) => {
     const updatedProductos = [...productosSeleccionados];
-    if(index >= 0 && index < updatedProductos.length){
+    if (index >= 0 && index < updatedProductos.length) {
       const producto = updatedProductos[index];
       //actualiza la cantidad de productos
       producto.cantidad = cantidad;
       //calcula el iva
-      const impuesto = producto.impuesto /100;
+      const impuesto = producto.impuesto / 100;
       const iva = producto.precio * impuesto * cantidad;
       producto.iva = iva;
       //calcula el total (precio por cantida + iva)
       const total = producto.precio * cantidad;
-      producto.total = total
+      producto.total = total;
       //actualiza el precio de producto selccionado
       setProductosSeleccionados(updatedProductos);
     }
-  }
-
+  };
 
   const eliminarProducto = (index) => {
     const updatedProductos = [...productosSeleccionados];
     updatedProductos.splice(index, 1); //elimana el producto en el indice dado
     setProductosSeleccionados(updatedProductos);
-  }
-
+  };
 
   // Calcular el total de la factura
   const totalFactura = productosSeleccionados.reduce((total, producto) => {
     return total + (producto.total || 0);
   }, 0);
 
-
-   // Calcular el total del IVA para la factura
-   const totalIVA = productosSeleccionados.reduce((total, producto) => {
+  // Calcular el total del IVA para la factura
+  const totalIVA = productosSeleccionados.reduce((total, producto) => {
     // Sumar el IVA de cada producto
     return total + (producto.iva || 0);
-  }, 0); 
-
+  }, 0);
 
   const precioBruto = totalFactura - totalIVA;
 
   const generarFactura = async () => {
     try {
-
       // Obtener el ObjectId del cliente
       const clienteId = cliente._id;
 
       // Obtener los productos seleccionados y los detalles del cliente
       const productosParaFactura = productosSeleccionados.map((producto) => ({
         producto: producto._id,
-        codigo:producto.codigo,
-        descripcion:producto.descripcion,        
+        codigo: producto.codigo,
+        descripcion: producto.descripcion,
         precioUnidad: producto.precio,
         cantidad: producto.cantidad,
-        iva: producto.iva, 
+        iva: producto.iva,
       }));
 
-  
       // Realizar la solicitud POST para guardar la factura en el servidor
-      const response = await axios.post('http://localhost:3000/api/factura', {
+      const response = await axios.post("http://localhost:3000/api/factura", {
         detalle: productosParaFactura,
         cliente: clienteId,
         totalNeto: totalFactura,
         numeroFactura: numActual,
-        
       });
-  
+
       console.log("Factura generada:", response.data);
-      
+
       // Limpiar la lista de productos seleccionados después de generar la factura
       setProductosSeleccionados([]); // Limpiar la lista de productos seleccionados
       setNitInput(""); // Limpiar el estado de nitInput (input de búsqueda de cliente)
-      setCliente({ // Limpiar los datos del cliente
-      nit: "",
-      nombre: "",
-      direccion: "",
-      telefono: ""
-    });
+      setCliente({
+        // Limpiar los datos del cliente
+        nit: "",
+        nombre: "",
+        direccion: "",
+        telefono: "",
+      });
       setBusqueda(""); // Limpiar el estado de busqueda (input de búsqueda de productos)
       setProductos([]); //elimina la tabla de resultado de busqueda
       obtenerNumero();
-  
+
       // Mostrar mensaje de éxito al usuario
       alert("¡Factura generada con éxito!");
     } catch (error) {
       console.error("Error al generar factura:", error);
-      
+
       // Mostrar mensaje de error al usuario
       alert("¡Error al generar la factura!");
     }
   };
-
 
   //esta funcion se encarga de que la funcion obtener fecha se ejecute y muestre
   //la fecha apenas se renderice la pagina
   useEffect(() => {
     obtenerFecha();
     mostrarNumeroActual();
+    obtenerEmpresa();
   }, []);
+
+  const handleSubmitC = (e) => {
+    e.preventDefault();
+    hasdleBuscarCliente();
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -270,6 +305,14 @@ function Facturacion({ nombre }) {
     <div className="containerPrincipal">
       <h1>Facturación</h1>
       <section className="seccion-datosfac numFactura">
+        {/* Aquí se muestra la información de la empresa */}
+        <div>
+          <h4>Información de la Empresa:</h4>
+          <p>NIT: {empresa.nit}</p>
+          <p>Nombre: {empresa.nombre}</p>
+          <p>Dirección: {empresa.direccion}</p>
+          <p>Teléfono: {empresa.telefono}</p>
+        </div>
         <div className="form-group">
           <label htmlFor="numeroFactura">Numero de Factura:</label>
           <input
@@ -291,60 +334,43 @@ function Facturacion({ nombre }) {
           />
         </div>
       </section>
-      <section className="secDatosCliente">        
+      <section className="secDatosCliente">
         <div className="datosClientes">
           <h2>datos del cliente </h2>
           <div className=" buscarCliente">
-            <div>
-              <label>nit del cliente</label>
-              <input
-                type="text"
-                className=" inputClienteID"
-                value={nitInput}
-                onChange={(e) => setNitInput(e.target.value)}            
-              />
-            </div>
-            <button
-              className="btn btn-success btnCliente"
-              onClick={hasdleBuscarCliente}
-            >
-              buscar cliente
-            </button>
+            <form className="forBuscar" onSubmit={handleSubmitC}>
+              <div className="inútbuscarCliente">                
+                  <label>NIT del cliente</label>
+                  <input
+                    type="text"
+                    className="inputClienteID"
+                    value={nitInput}
+                    onChange={(e) => setNitInput(e.target.value)}
+                  />                
+              </div>
+              <div className="contenedorPrecio">
+                <h3>total factura</h3>
+                <p>
+                  {totalFactura.toLocaleString("es-CO", {
+                    style: "currency",
+                    currency: "COP",
+                    minimumFractionDigits: 0,
+                    maximumFractionDigits: 0,
+                  })}
+                </p>
+              </div>
+            </form>
           </div>
           <div className=" contenedorCliente">
-            
-              <div >
-                <label className="lableCliente">nombre</label>
-                <input
-                  type="text"
-                  className="inputCliente "
-                  value={cliente.nombre}
-                  onChange={(e) => setCliente({...cliente, nombre: e.target.value})}
-                  disabled
-                />
-              </div>
-              <div >
-                <label className="lableCliente">direccion</label>
-                <input
-                  type="text"
-                  className="inputCliente"
-                  value={cliente.direccion}
-                  onChange={(e) => setCliente({...cliente, direccion: e.target.value})}
-                  disabled
-                />
-              </div >
-              <div >
-              <label className="lableCliente">telefono</label>
-                <input
-                  type="text"
-                  className=" inputCliente"
-                  value={cliente.telefono}
-                  onChange={(e) => ({...cliente, telefono: e.target.value})}
-                  disabled
-                />
-              </div>
-            
+            <div>
+              <p>{cliente.nombre}</p>
+
+              <p>{cliente.direccion}</p>
+
+              <p>{cliente.telefono}</p>
+            </div>
           </div>
+          
         </div>
       </section>
 
@@ -353,7 +379,7 @@ function Facturacion({ nombre }) {
         <form onSubmit={handleSubmit}>
           <div>
             <div>
-              <label>ID PRODUCTO</label>
+              <label>DESCRIPCION PRODUCTO</label>
               <input
                 type="text"
                 className="inputProd"
@@ -379,24 +405,24 @@ function Facturacion({ nombre }) {
               </tr>
             </thead>
             <tbody>
-              {productos.map((producto) =>(
+              {productos.map((producto) => (
                 <tr key={producto.id}>
                   <td>{producto.codigo}</td>
                   <td>{producto.descripcion}</td>
                   <td>{producto.precio}</td>
                   <td>{producto.cantidad}</td>
                   <td>
-                    <button className="btn btn-success"
-                    onClick={() => agregarProducto(producto)}>
+                    <button
+                      className="btn btn-success"
+                      onClick={() => agregarProducto(producto)}
+                    >
                       agregar
                     </button>
                   </td>
-
                 </tr>
               ))}
             </tbody>
           </table>
-
         </div>
       </section>
       <section className="productosSelec">
@@ -404,7 +430,7 @@ function Facturacion({ nombre }) {
         <table>
           <thead>
             <tr>
-            <th>Código</th>
+              <th>Código</th>
               <th>Descripción</th>
               <th>Cantidad</th>
               <th>Precio Unitario</th>
@@ -423,7 +449,9 @@ function Facturacion({ nombre }) {
                     type="number"
                     className="form-control"
                     value={producto.cantidad}
-                    onChange={(e) => handleCantidadChange(index, parseInt(e.target.value, 10))}
+                    onChange={(e) =>
+                      handleCantidadChange(index, parseInt(e.target.value, 10))
+                    }
                   />
                 </td>
                 <td>${producto.precio}</td>
@@ -442,17 +470,43 @@ function Facturacion({ nombre }) {
           </tbody>
           <tfoot>
             <tr>
-              <td colSpan="4"><strong>Total de la Factura:</strong></td>              
-              <td>total bruto: {precioBruto.toLocaleString('es-CO', { style: 'currency', currency: 'COP', minimumFractionDigits: 0, maximumFractionDigits: 0 })}</td>
-              <td>total iva: {totalIVA.toLocaleString('es-CO', { style: 'currency', currency: 'COP', minimumFractionDigits: 0, maximumFractionDigits: 0 })}</td>
-              <td>valor neto: {totalFactura.toLocaleString('es-CO', { style: 'currency', currency: 'COP', minimumFractionDigits: 0, maximumFractionDigits: 0 })}</td>
+              <td colSpan="4">
+                <strong>Total de la Factura:</strong>
+              </td>
+              <td>
+                total bruto:{" "}
+                {precioBruto.toLocaleString("es-CO", {
+                  style: "currency",
+                  currency: "COP",
+                  minimumFractionDigits: 0,
+                  maximumFractionDigits: 0,
+                })}
+              </td>
+              <td>
+                total iva:{" "}
+                {totalIVA.toLocaleString("es-CO", {
+                  style: "currency",
+                  currency: "COP",
+                  minimumFractionDigits: 0,
+                  maximumFractionDigits: 0,
+                })}
+              </td>
+              <td>
+                valor neto:{" "}
+                {totalFactura.toLocaleString("es-CO", {
+                  style: "currency",
+                  currency: "COP",
+                  minimumFractionDigits: 0,
+                  maximumFractionDigits: 0,
+                })}
+              </td>
             </tr>
-          </tfoot>          
+          </tfoot>
         </table>
         <button className="btn btn-success btnGenerar" onClick={generarFactura}>
-            Generar Factura
-          </button>
-      </section>  
+          Generar Factura
+        </button>
+      </section>
     </div>
   );
 }
